d6a3920e05f4a71344f0e40e1a78abb006fc5861
AllisonLucca <allison.d.lucca@gmail.com>
2019-02-24 23:50:48 -0700

    Added upto method table

--- jvpm/ClassFile.py
+++ jvpm/ClassFile.py
@@ -1,13 +1,21 @@
 class ConstantInfo():
-    tag = 0
-    info = []
-    name_index = 0
+    def __init__(self):
+        self.tag = 0
+        self.info = []
+        self.name_index = 0
+
+class methodInfo():
+    def __init__(self):
+        self.value = 0
 
 class ClassFile():
     def __init__(self):
         with open('test.class', 'rb') as binary_file:
             self.data = binary_file.read()
         self.c_pool_table = []
+        self.cpoolsize = 0
+        self.interface_table = []
+        self.method_table= []
 
     def get_magic(self):
         magic = ""
@@ -24,7 +32,7 @@ class ClassFile():
     def get_constant_pool_count(self):
         return self.data[8] + self.data[9]
 
-    def get_constant_pool_size(self):
+    def create_c_pool(self):
         index_offset = 10
         switch = {
             3: 4,
@@ -44,38 +52,61 @@ class ClassFile():
         max = int(self.get_constant_pool_count()) - 1
         for i in range (0,max):
             thing = ConstantInfo()
-            thing.tag = self.data[i + index_offset]
+            thing.tag = self.data[index_offset]
             index_offset += 1
             if thing.tag == 1:
-                bytesNeeded = self.data[i + index_offset] + self.data[i + index_offset + 1]
+                bytesNeeded = self.data[index_offset] + self.data[index_offset + 1]
                 index_offset += 2
             else:
                 bytesNeeded = switch.get(thing.tag)
             for x in range (0,bytesNeeded):
-                thing.info.append(self.data[i + index_offset])
+                thing.info.append(self.data[x + index_offset])
                 index_offset += 1
+            print("Constant #", i, " tag: ", thing.tag, " value: ", thing.info)
             self.c_pool_table.append(thing)
-            index_offset -= 1
-        print(index_offset)
-        return index_offset
+        self.cpoolsize = index_offset - 10
+        return index_offset - 10
+
+    def get_constant_pool_size(self):
+        if(len(self.c_pool_table)!=self.get_constant_pool_count()-1):
+            self.create_c_pool()
+        return self.cpoolsize
 
     def get_flags(self):
-        offset = self.get_constant_pool_count()
-        return self.data[offset] + self.data[offset+1]
+        return self.data[10+self.get_constant_pool_size()] + self.data[self.get_constant_pool_size()+11]
 
     def get_this_class(self):
-        offset = self.get_constant_pool_count()
-        return self.data[offset+2] + self.data[offset+3]
+        return self.data[self.get_constant_pool_size()+12] + self.data[self.get_constant_pool_size()+13]
 
     def get_super_class(self):
-        offset = self.get_constant_pool_count()
-        return self.data[offset+4] + self.data[offset+5]
+        return self.data[self.get_constant_pool_size()+14] + self.data[self.get_constant_pool_size()+15]
 
     def get_interface_count(self):
-        offset = self.get_constant_pool_count()
-        return self.data[offset+6] + self.data[offset+7]
+        return self.data[self.get_constant_pool_size()+16] + self.data[self.get_constant_pool_size()+17]
 
+    def create_interface(self):
+        itable = [self.get_interface_count()]
+        for i in range[0,len(itable)]:
+            itable[i] = self.data[self.get_constant_pool_size() + 18 + i]
+        self.interface_table = itable 
 
+    def get_field_count(self):
+        return self.data[18+self.get_constant_pool_size()+self.get_interface_count()]
+
+    def create_field_table(self):
+        '''dont wanna do'''
+        return 
+    
+    def get_field_size(self):
+        return self.get_field_count()*2
+
+    def get_method_count(self):
+        return self.data[20+self.get_constant_pool_size() + self.get_interface_count() + self.get_field_size()] + self.data[21+self.get_constant_pool_size() + self.get_interface_count() + self.get_field_size()]
+
+    def create_method_table(self):
+        mtable = methodInfo()
+        for i in range(0, self.get_method_count()):
+            print("I have no clue")
     
 
 
@@ -95,5 +126,5 @@ if '__main__' == __name__:
     print('minor_version: ', java.get_minor()) #pragma: no cover
     print('major_version: ', java.get_major()) #pragma: no cover
     print('constant_pool_count: ', java.get_constant_pool_count()) #pragma: no cover
-    #print('interface count: ' + java.get_interface_count())
     print('interface count: ', java.get_interface_count())
+    print('method count: ', java.get_method_count())
