24fd9eb487d4f3ec4215b7f71da9142a8ee4c876
Eric <gms_eric22@yahoo.com>
2019-03-20 09:59:54 -0600

    Removed unnessary lines

--- jvpm/test/test_ClassFile.py
+++ jvpm/test/test_ClassFile.py
@@ -200,9 +200,6 @@ class TestOpCodes(unittest.TestCase):
         m.op_stack.append(4)
         m.interpret(0x36, [4])
         self.assertEqual(m.lva[4], 4)
-        m.op_stack.append(4)
-        m.interpret(0x36, [4])
-        self.assertEqual(m.lva[4], 5)
 
     def test_istore_0(self):
         m = OpCodes()
