eeaf16f5189743891ce1dc6932fa7d810673c323
Eric <gms_eric22@yahoo.com>
2019-05-06 13:48:45 -0600

    reformatted using black

--- jvpm/OpCodes.py
+++ jvpm/OpCodes.py
@@ -3,45 +3,91 @@ import re
 import numpy as np
 
 
-class OpCodes():
+class OpCodes:
     def __init__(self):
         self._op_stack = []  # operand stack for the opcodes
         self._lva = []  # local variable array initialized
-        self._table = {0x00: self._not_implemented,
-                       0x02: self._iconst_m1, 0x03: self._iconst_0, 0x04: self._iconst_1,
-                       0x05: self._iconst_2, 0x06: self._iconst_3,
-                       0x07: self._iconst_4, 0x08: self._iconst_5,
-                       0x60: self._iadd, 0x7e: self._iand, 0x6c: self._idiv,
-                       0x68: self._imul, 0x74: self._ineg, 0x80: self._ior,
-                       0x70: self._irem, 0x78: self._ishl,
-                       0x7a: self._ishr, 0x64: self._isub, 0x7c: self._iushr,
-                       0x82: self._ixor, 0x15: self._iload,
-                       0x1a: self._iload_0, 0x1b: self._iload_1,
-                       0x1c: self._iload_2, 0x1d: self._iload_3,
-                       0x36: self._istore, 0x3b: self._istore_0,
-                       0x3c: self._istore_1, 0x3d: self._istore_2,
-                       0x3e: self._istore_3, 0x91: self._i2b, 0x92: self._i2c,
-                       0x87: self._i2d, 0x86: self._i2f,
-                       0x85: self._i2l, 0x93: self._i2s,
-                       0xb6: self._invokevirtual, 0xb2: self._getstatic, 0x12: self._ldc,
-                       0x8b: self._f2i, 0x8c: self._f2l,
-                       0x8d: self._f2d, 0xb1: self._ret, 0xb: self._fconst_0,
-                       0xc: self._fconst_1, 0xd: self._fconst_2,
-                       0x17: self._fload, 0x22: self._fload_0, 0x23: self._fload_1,
-                       0x24: self._fload_2, 0x25: self._fload_3,
-                       0x1e: self._lload_0, 0x1f: self._lload_1,
-                       0x20:self._lload_2, 0x21:self._lload_3, 0x16:self._lload,
-                       0x9: self._lconst_0, 0xa: self._lconst_1,
-                       0x3f: self._lstore_0, 0x40: self._lstore_1,
-                       0x41: self._lstore_2, 0x42: self._lstore_3,
-                       0x37: self._lstore, 0x61: self._ladd, 0x65: self._lsub,
-                       0x69: self._lmul, 0x6d: self._ldiv,
-                       0x71: self._lrem, 0x75: self._lneg, 0x7d: self._lushr,
-                       0x7f: self._land, 0x81: self._lor,
-                       0x83: self._lxor, 0x88: self._l2i, 0x89: self._l2f, 0x8a: self._l2d}
+        self._table = {
+            0x00: self._not_implemented,
+            0x02: self._iconst_m1,
+            0x03: self._iconst_0,
+            0x04: self._iconst_1,
+            0x05: self._iconst_2,
+            0x06: self._iconst_3,
+            0x07: self._iconst_4,
+            0x08: self._iconst_5,
+            0x60: self._iadd,
+            0x7E: self._iand,
+            0x6C: self._idiv,
+            0x68: self._imul,
+            0x74: self._ineg,
+            0x80: self._ior,
+            0x70: self._irem,
+            0x78: self._ishl,
+            0x7A: self._ishr,
+            0x64: self._isub,
+            0x7C: self._iushr,
+            0x82: self._ixor,
+            0x15: self._iload,
+            0x1A: self._iload_0,
+            0x1B: self._iload_1,
+            0x1C: self._iload_2,
+            0x1D: self._iload_3,
+            0x36: self._istore,
+            0x3B: self._istore_0,
+            0x3C: self._istore_1,
+            0x3D: self._istore_2,
+            0x3E: self._istore_3,
+            0x91: self._i2b,
+            0x92: self._i2c,
+            0x87: self._i2d,
+            0x86: self._i2f,
+            0x85: self._i2l,
+            0x93: self._i2s,
+            0xB6: self._invokevirtual,
+            0xB2: self._getstatic,
+            0x12: self._ldc,
+            0x8B: self._f2i,
+            0x8C: self._f2l,
+            0x8D: self._f2d,
+            0xB1: self._ret,
+            0xB: self._fconst_0,
+            0xC: self._fconst_1,
+            0xD: self._fconst_2,
+            0x17: self._fload,
+            0x22: self._fload_0,
+            0x23: self._fload_1,
+            0x24: self._fload_2,
+            0x25: self._fload_3,
+            0x1E: self._lload_0,
+            0x1F: self._lload_1,
+            0x20: self._lload_2,
+            0x21: self._lload_3,
+            0x16: self._lload,
+            0x9: self._lconst_0,
+            0xA: self._lconst_1,
+            0x3F: self._lstore_0,
+            0x40: self._lstore_1,
+            0x41: self._lstore_2,
+            0x42: self._lstore_3,
+            0x37: self._lstore,
+            0x61: self._ladd,
+            0x65: self._lsub,
+            0x69: self._lmul,
+            0x6D: self._ldiv,
+            0x71: self._lrem,
+            0x75: self._lneg,
+            0x7D: self._lushr,
+            0x7F: self._land,
+            0x81: self._lor,
+            0x83: self._lxor,
+            0x88: self._l2i,
+            0x89: self._l2f,
+            0x8A: self._l2d,
+        }
 
     def _not_implemented(self):
-        return 'not implemented'
+        return "not implemented"
 
     def interpret(self, value, operands=None, constants=None):
         """
@@ -97,9 +143,9 @@ class OpCodes():
         value2 = self._op_stack.pop()
         value1 = self._op_stack.pop()
         try:
-            self._op_stack.append(value1//value2)
+            self._op_stack.append(value1 // value2)
         except ZeroDivisionError:
-            return 'Error: Divides by Zero'
+            return "Error: Divides by Zero"
 
     def _imul(self):
         value2 = self._op_stack.pop()
@@ -120,7 +166,7 @@ class OpCodes():
         try:
             self._op_stack.append(value1 % value2)
         except ZeroDivisionError:
-            return 'Error: Divides by Zero'
+            return "Error: Divides by Zero"
 
     def _ishl(self):
         value2 = self._op_stack.pop()
@@ -140,7 +186,7 @@ class OpCodes():
     def _iushr(self):
         value2 = self._op_stack.pop()
         value1 = self._op_stack.pop()
-        s = value2 & 0x1f
+        s = value2 & 0x1F
         if value1 >= 0:
             self._op_stack.append(value1 >> s)
         else:
@@ -249,7 +295,7 @@ class OpCodes():
     def _lload(self, operands):
         index = operands.pop()
         frag1 = self._lva[index]
-        frag2 = self._lva[index+1]
+        frag2 = self._lva[index + 1]
         self._op_stack.append(frag1)
         self._op_stack.append(frag2)
 
@@ -375,7 +421,6 @@ class OpCodes():
         self._op_stack.append(answer1)
         self._op_stack.append(answer2)
 
-
     def _lrem(self):
         second_op2 = self._op_stack.pop()
         second_op1 = self._op_stack.pop()
@@ -400,7 +445,7 @@ class OpCodes():
     def _lushr(self):
         value2 = self._op_stack.pop()
         value1 = self._op_stack.pop()
-        s = value2 & 0x3f
+        s = value2 & 0x3F
         if value1 >= 0:
             self._op_stack.append(value1 >> s)
         else:
@@ -469,9 +514,9 @@ class OpCodes():
             val = self._get_str_from_cpool(class_index, c_pool)
 
             if const_ref.tag == 10:
-                val += '.'
+                val += "."
             elif const_ref.tag == 12:
-                val += ':'
+                val += ":"
 
             if const_ref.info.__len__() > 2:
                 name_type_index = const_ref.info[2] + const_ref.info[3] - 1
@@ -486,11 +531,11 @@ class OpCodes():
         num1 = operands.pop()
         num2 = operands.pop()
         method = self._get_str_from_cpool(num1 + num2 - 1, c_pool)
-        if method == 'java/io/PrintStream.println:(I)V':
+        if method == "java/io/PrintStream.println:(I)V":
             print(self._op_stack.pop())
-        elif method == 'java/io/PrintStream.println:(Ljava/lang/String;)V':
+        elif method == "java/io/PrintStream.println:(Ljava/lang/String;)V":
             print(self._op_stack.pop())
-        elif method == 'java/util/Scanner.nextInt:()I':
+        elif method == "java/util/Scanner.nextInt:()I":
             data = input("Enter a number: ")
             while re.match(r"[-+]?\d+$", data) is None:
                 print("Invalid input")
@@ -510,14 +555,14 @@ class OpCodes():
     # Splits long in half and returns first and second frag as int32
     def _longsplit(self, val):
         val = np.int64(val)
-        frag2 = np.int32(val & 0x00000000ffffffff)
-        frag1 = np.int32((val >> 32) & 0x00000000ffffffff)
+        frag2 = np.int32(val & 0x00000000FFFFFFFF)
+        frag1 = np.int32((val >> 32) & 0x00000000FFFFFFFF)
         return frag1, frag2
 
     # Takes two fragments and combines them, returning a 64 bit int
     def _longcomb(self, frag1, frag2):
-        frag1 = np.int64((0x00000000ffffffff & frag1) << 32)
-        frag2 = np.int64(0x00000000ffffffff & frag2)
+        frag1 = np.int64((0x00000000FFFFFFFF & frag1) << 32)
+        frag2 = np.int64(0x00000000FFFFFFFF & frag2)
         return frag1 + frag2
 
     def _fconst_0(self):
@@ -546,23 +591,23 @@ class OpCodes():
         self._op_stack.append(self._lva[3])
 
     def _f2i(self):
-        value1 = struct.unpack('!f', bytes.fromhex(self._op_stack.pop()))[0]
+        value1 = struct.unpack("!f", bytes.fromhex(self._op_stack.pop()))[0]
         self._op_stack.append(np.int32(value1))
 
     def _f2l(self):
-        value1 = np.int64(struct.unpack('!f', bytes.fromhex(self._op_stack.pop()))[0])
+        value1 = np.int64(struct.unpack("!f", bytes.fromhex(self._op_stack.pop()))[0])
         value2 = np.right_shift(value1, 32)
         value3 = np.bitwise_and(value1, 0x00000000FFFFFFFF)
         self._op_stack.append(np.int32(value2))
         self._op_stack.append(np.int32(value3))
 
     def _f2d(self):
-        value1 = np.float64(struct.unpack('!f', bytes.fromhex(self._op_stack.pop()))[0])
-        hexval = hex(struct.unpack('<Q', struct.pack('<d', value1))[0])
+        value1 = np.float64(struct.unpack("!f", bytes.fromhex(self._op_stack.pop()))[0])
+        hexval = hex(struct.unpack("<Q", struct.pack("<d", value1))[0])
         value2 = hexval[2:10]
         value3 = hexval[10:18]
         self._op_stack.append(int(value2, 16))
         self._op_stack.append(int(value3, 16))
 
     def _ret(self):
-        return ''
+        return ""
