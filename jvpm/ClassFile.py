0f179fe031e6f9e8e0d520db502bbf5690c46718
Eric <gms_eric22@yahoo.com>
2019-05-07 14:11:15 -0600

    Updated private methods and variables for pylint

--- jvpm/ClassFile.py
+++ jvpm/ClassFile.py
@@ -1,18 +1,21 @@
 from jvpm.OpCodes import OpCodes
 
-class ConstantInfo():
+
+class ConstantInfo:
     def __init__(self):
         self.tag = 0
         self.info = []
         self.name_index = 0
 
-class MethodInfo():
+
+class MethodInfo:
     def __init__(self):
         self.access_flags = 0
         self.name_index = 0
         self.descriptor_index = 0
 
-class CodeAttribute():
+
+class CodeAttribute:
     def __init__(self):
         self.attribute_name_index = 0
         self.attribute_length = 0
@@ -21,34 +24,35 @@ class CodeAttribute():
         self.code_length = 0
         self.code = []
 
-class ClassFile():
+
+class ClassFile:
     def __init__(self, path):
-        with open(path, 'rb') as binary_file:
+        with open(path, "rb") as binary_file:
             self.data = binary_file.read()
         self.c_pool_table = []
         self.cpoolsize = 0
-        #self.interface_table = []
+        # self.interface_table = []
         self.method_table = []
         self.attribute_table = []
-        self.parse_class_file()
+        self._parse_class_file()
 
     def _parse_class_file(self):
-        self.get_magic()
-        self.get_minor()
-        self.get_major()
-        self.get_constant_pool_count()
-        self.create_c_pool()
-        self.get_interface_count()
-        self.get_field_count()
-        self.get_method_count()
-        self.create_method_table()
-        self.get_attribute_count()
-        self.create_attribute_table()
+        self._get_magic()
+        self._get_minor()
+        self._get_major()
+        self._get_constant_pool_count()
+        self._create_c_pool()
+        self._get_interface_count()
+        self._get_field_count()
+        self._get_method_count()
+        self._create_method_table()
+        self._get_attribute_count()
+        self._create_attribute_table()
 
     def _get_magic(self):
         magic = ""
         for i in range(4):
-            magic += format(self.data[i], '02X')
+            magic += format(self.data[i], "02X")
         return magic
 
     def _get_minor(self):
@@ -75,90 +79,143 @@ class ClassFile():
             12: 4,
             15: 3,
             16: 2,
-            18: 4
+            18: 4,
         }
-        max = int(self.get_constant_pool_count()) - 1
-        for i in range (0,max):
+        maximum = int(self._get_constant_pool_count()) - 1
+        for i in range(0, maximum):
             thing = ConstantInfo()
             thing.tag = self.data[index_offset]
             index_offset += 1
             if thing.tag == 1:
-                bytesNeeded = self.data[index_offset] + self.data[index_offset + 1]
+                bytes_needed = self.data[index_offset] + self.data[index_offset + 1]
                 index_offset += 2
             else:
-                bytesNeeded = switch.get(thing.tag)
-            for x in range (0,bytesNeeded):
+                bytes_needed = switch.get(thing.tag)
+            for x in range(0, bytes_needed):
                 thing.info.append(self.data[x + index_offset])
-            index_offset += bytesNeeded
-            #print("Constant #", i, " tag: ", thing.tag, " value: ", thing.info)
+            index_offset += bytes_needed
+            # print("Constant #", i, " tag: ", thing.tag, " value: ", thing.info)
             self.c_pool_table.append(thing)
         self.cpoolsize = index_offset - 10
         return index_offset - 10
 
     def _get_constant_pool_size(self):
-        if(len(self.c_pool_table)!=self.get_constant_pool_count()-1):
-            self.create_c_pool()
+        if len(self.c_pool_table) != self._get_constant_pool_count() - 1:
+            self._create_c_pool()
         return self.cpoolsize
 
     def _get_flags(self):
-        return self.data[10+self.get_constant_pool_size()] + self.data[self.get_constant_pool_size()+11]
+        return (
+            self.data[10 + self._get_constant_pool_size()]
+            + self.data[self._get_constant_pool_size() + 11]
+        )
 
     def _get_this_class(self):
-        return self.data[self.get_constant_pool_size()+12] + self.data[self.get_constant_pool_size()+13]
+        return (
+            self.data[self._get_constant_pool_size() + 12]
+            + self.data[self._get_constant_pool_size() + 13]
+        )
 
     def _get_super_class(self):
-        return self.data[self.get_constant_pool_size()+14] + self.data[self.get_constant_pool_size()+15]
+        return (
+            self.data[self._get_constant_pool_size() + 14]
+            + self.data[self._get_constant_pool_size() + 15]
+        )
 
     def _get_interface_count(self):
-        return self.data[self.get_constant_pool_size()+16] + self.data[self.get_constant_pool_size()+17]
+        return (
+            self.data[self._get_constant_pool_size() + 16]
+            + self.data[self._get_constant_pool_size() + 17]
+        )
 
-    #def create_interface(self):
+    # def create_interface(self):
     #    itable = [self.get_interface_count()]
     #    for i in range[0,len(itable)]:
     #        itable[i] = self.data[self.get_constant_pool_size() + 18 + i]
     #    self.interface_table = itable
 
     def _get_field_count(self):
-        return self.data[18+self.get_constant_pool_size()+self.get_interface_count()] + self.data[19+self.get_constant_pool_size()+self.get_interface_count()]
+        return (
+            self.data[18 + self._get_constant_pool_size() + self._get_interface_count()]
+            + self.data[19 + self._get_constant_pool_size() + self._get_interface_count()]
+        )
 
-    #def create_field_table(self):
+    # def create_field_table(self):
     #    '''dont wanna do'''
     #    return
-    
+
     def _get_field_size(self):
-        return self.get_field_count()*2
+        return self._get_field_count() * 2
 
     def _get_method_count(self):
-        return self.data[20+self.get_constant_pool_size() + self.get_interface_count() + self.get_field_size()] + self.data[21+self.get_constant_pool_size() + self.get_interface_count() + self.get_field_size()]
+        return (
+            self.data[
+                20
+                + self._get_constant_pool_size()
+                + self._get_interface_count()
+                + self._get_field_size()
+            ]
+            + self.data[
+                21
+                + self._get_constant_pool_size()
+                + self._get_interface_count()
+                + self._get_field_size()
+            ]
+        )
 
     def _create_method_table(self):
-        count = 22+self.get_constant_pool_size() + self.get_interface_count() + self.get_field_size()
-        for i in range(0, self.get_method_count()):
+        count = (
+            22
+            + self._get_constant_pool_size()
+            + self._get_interface_count()
+            + self._get_field_size()
+        )
+        for i in range(0, self._get_method_count()):
             mtable = MethodInfo()
-            mtable.access_flags = self.data[count] + self.data[1+count]
-            mtable.name_index = self.data[2+count] + self.data[3 + count]
-            mtable.descriptor_index = self.data[4+count] + self.data[5 + count]
+            mtable.access_flags = self.data[count] + self.data[1 + count]
+            mtable.name_index = self.data[2 + count] + self.data[3 + count]
+            mtable.descriptor_index = self.data[4 + count] + self.data[5 + count]
             self.method_table.append(mtable)
         return self.method_table
 
     def _get_attribute_count(self):
-        count = 28 + self.get_constant_pool_size() + self.get_interface_count() + self.get_field_size()
+        count = (
+            28
+            + self._get_constant_pool_size()
+            + self._get_interface_count()
+            + self._get_field_size()
+        )
         return self.data[count] + self.data[1 + count]
 
     def _create_attribute_table(self):
-        count = 30 + self.get_constant_pool_size() + self.get_interface_count() + self.get_field_size()
-        for i in range(0, self.get_attribute_count()):
-            codeAtt = CodeAttribute()
-            codeAtt.attribute_name_index = self.data[count] + self.data[1 + count]
-            codeAtt.attribute_length = self.data[2 + count] + self.data[3 + count] + self.data[4 + count] + self.data[5 + count]
-            codeAtt.max_stack = self.data[6 + count] + self.data[7 + count]
-            codeAtt.max_locals = self.data[8 + count] + self.data[9 + count]
-            codeAtt.code_length = self.data[10 + count] + self.data[11 + count] + self.data[12 + count] + self.data[13 + count]
+        count = (
+            30
+            + self._get_constant_pool_size()
+            + self._get_interface_count()
+            + self._get_field_size()
+        )
+        for i in range(0, self._get_attribute_count()):
+            code_att = CodeAttribute()
+            code_att.attribute_name_index = self.data[count] + self.data[1 + count]
+            code_att.attribute_length = (
+                self.data[2 + count]
+                + self.data[3 + count]
+                + self.data[4 + count]
+                + self.data[5 + count]
+            )
+            code_att.max_stack = self.data[6 + count] + self.data[7 + count]
+            code_att.max_locals = self.data[8 + count] + self.data[9 + count]
+            code_att.code_length = (
+                self.data[10 + count]
+                + self.data[11 + count]
+                + self.data[12 + count]
+                + self.data[13 + count]
+            )
             count = count + 13
-            for i in range(0, codeAtt.code_length):
-                codeAtt.code.append(self.data[count + 1])
+            for i in range(0, code_att.code_length):
+                code_att.code.append(self.data[count + 1])
                 count += 1
-            self.attribute_table.append(codeAtt)
+            self.attribute_table.append(code_att)
         return self.attribute_table
 
     def run_opcodes(self):
@@ -173,19 +230,30 @@ class ClassFile():
                 value = self.attribute_table[table_index].code[code_index]
                 if value == 54 or value == 21 or value == 0x17:
                     code_index += 1
-                    ops.interpret(value, [self.attribute_table[table_index].code[code_index]])
+                    ops.interpret(
+                        value, [self.attribute_table[table_index].code[code_index]]
+                    )
                 elif value == 0x12:
                     code_index += 1
-                    ops.interpret(value, [self.attribute_table[table_index].code[code_index]], self.c_pool_table)
-                elif value == 0xb6 or value == 0xb2:
+                    ops.interpret(
+                        value,
+                        [self.attribute_table[table_index].code[code_index]],
+                        self.c_pool_table,
+                    )
+                elif value == 0xB6 or value == 0xB2:
                     code_index += 2
-                    ops.interpret(value, [self.attribute_table[table_index].code[code_index-1], self.attribute_table[table_index].code[code_index]], self.c_pool_table)
+                    ops.interpret(
+                        value,
+                        [
+                            self.attribute_table[table_index].code[code_index - 1],
+                            self.attribute_table[table_index].code[code_index],
+                        ],
+                        self.c_pool_table,
+                    )
                 else:
                     ops.interpret(value)
-                #print("stack: ", ops.op_stack)
-                #print("array: ", ops.lva)
+                # print("stack: ", ops.op_stack)
+                # print("array: ", ops.lva)
                 code_index += 1
             table_index += 1
         return ops
-
-
